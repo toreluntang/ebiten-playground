2b0534bcd55c53d30c48cc52ffb6f099582dea17
toreluntang <=>
2025-03-26 10:01:16 +0100

    Spawn food randomly up to a max

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"image/color"
 	"log"
+	"math"
+	"math/rand/v2"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
@@ -30,6 +32,7 @@ func (f *food) draw(screen *ebiten.Image) {
 }
 
 func (g *Game) Update() error {
+	g.randSpawnFood()
 	return nil
 }
 
@@ -44,6 +47,28 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeigh
 	return 320, 240
 }
 
+func (g *Game) randSpawnFood() {
+	// Do not add infinite food
+	if len(g.food) >= g.maxFood {
+		return
+	}
+
+	if rand.IntN(100) > 1 {
+		size := 16
+		randX := rand.IntN(320) - size
+		randY := rand.IntN(230) - size
+		x := math.Max(0, float64(randX))
+		y := math.Max(0, float64(randY))
+
+		g.addFood(
+			food{
+				imgPath: "assets/asset-pack/Items/Food/Fish.png",
+				x:       x,
+				y:       y,
+			})
+	}
+}
+
 // addFood adds a food item to the game along with its collider for convenience
 func (g *Game) addFood(f food) {
 	g.food = append(g.food, f)
@@ -53,6 +78,7 @@ func main() {
 	ebiten.SetWindowSize(640, 480)
 	ebiten.SetWindowTitle("Hello, World!")
 	if err := ebiten.RunGame(&Game{
+		maxFood: 10,
 		food: []food{
 			{
 				imgPath: "assets/asset-pack/Items/Food/Fish.png",
